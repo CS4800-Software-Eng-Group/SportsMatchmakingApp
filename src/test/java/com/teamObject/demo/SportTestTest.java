b8f95576578ef20c0a59ed595583c9e7aa203fe5
lilitarakelyan <69986950+lilitarakelyan@users.noreply.github.com>
2020-11-07 22:02:12 -0800

    JUnit

---
+++ src/test/java/com/teamObject/demo/SportTestTest.java
@@ -0,0 +1,22 @@
+package com.teamObject.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SportTestTest {
+
+	private SportTest t = new SportTest();
+	
+	@Test
+	public void test() {
+		t = new SportTest ();
+	}
+public void testtoSpring()
+{
+	String res = s.toString();
+	Assertions.assrtEquals("Sport [Sport ="+ Football"])
+	
+}
+}
