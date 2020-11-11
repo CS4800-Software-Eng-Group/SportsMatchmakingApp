aaa149343dcc3aa454a6227a4d65c698cd38b1d4
Nathan McCausland <naphidmc@gmail.com>
2020-11-10 19:38:56 -0800

    Allow h2 database console to be accessed from remote

--- src/main/java/com/example/demo/SpringsportsApplication.java
+++ src/main/java/com/example/demo/SpringsportsApplication.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import java.sql.SQLException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class SpringsportsApplication {
@@ -10,4 +14,8 @@ public class SpringsportsApplication {
 		SpringApplication.run(SpringsportsApplication.class, args);
 	}
 
+	@Bean(initMethod="start",destroyMethod="stop")
+	public org.h2.tools.Server h2WebConsoleServer () throws SQLException {
+		return org.h2.tools.Server.createWebServer("-web","-webAllowOthers","-webDaemon","-webPort", "8080");
+	}
 }
